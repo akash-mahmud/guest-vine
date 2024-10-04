52e6b1529bd8d9a016ca4265a042775d40907f66
Akash <mahmudakash177@gmail.com>
2024-10-04 20:11:44 +0600

    navbar creation

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,9 +1,40 @@
 "use client"
-import {UserButton} from '@clerk/nextjs'
+import { UserButton, useAuth } from '@clerk/nextjs'
+import Container from '../Container'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { Button } from '../ui/button'
 export default function NavBar() {
+  const router = useRouter()
+  const { userId } = useAuth()
   return (
-    <div>
-        <UserButton afterSwitchSessionUrl='/'/>
+    <div className=' sticky top-0 border border-b-primary/10 bg-secondary'>
+      <Container>
+        <div className='flex justify-between items-center'>
+
+          <div className='flex items-center gap-1 cursor-pointer' onClick={() => router.push("/")}>
+            <Image src={"/logo.png"} width={30} height={30} alt='logo' />
+            <div className=' font-bold text-xl'>
+              GuestVine
+            </div>
+          </div>
+          <div className='flex gap-3 items-center'>
+            <div>Theme</div>
+            <UserButton afterSwitchSessionUrl='/' />
+            {
+              !userId && <>
+
+                <Button onClick={() => router.push("/sign-in")} variant={'outline'} size={'sm'}>
+                  Sign in
+                </Button>
+                <Button onClick={() => router.push("/sign-up")} size={'sm'}>
+                  Sign up
+                </Button>
+              </>
+            }
+          </div>
+        </div>
+      </Container>
     </div>
   )
 }
